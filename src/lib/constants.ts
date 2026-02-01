5bc8164e0ee9af3579e4828f580f09554f3cf9bb
Sahil Arora <iamsahil@protonmail.com>
2026-02-01 20:07:12 +0530

    Add files via upload

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+export const LINKS = {
+  github: "https://github.com/sahilarora3117",
+  linkedin: "https://www.linkedin.com/in/sahilarora3117/",
+  email: "iamsahil@protonmail.com",
+  website: "https://sahilarora.dev"
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
